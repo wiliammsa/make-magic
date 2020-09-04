f06072dae883295148c80587ca8b43084762dfa7
Wiliam Mendes <wiliammsa@gmail.com>
2020-09-03 22:40:53 -0300

    Criação de model e migration

---
+++ www/api/app/Models/Character.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Character extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['name', 'role', 'school', 'house', 'patronus'];
+    protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
+}
---
+++ www/api/database/migrations/2020_09_04_005941_create_characters_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCharactersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->string('id', 50);
+            $table->string('name', 100);
+            $table->string('role', 20);
+            $table->string('school', 100);
+            $table->string('house', 50);
+            $table->string('patronus', 100);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('characters');
+    }
+}
