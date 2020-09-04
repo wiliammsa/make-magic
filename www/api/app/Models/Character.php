503248130fa06775914b9d2019016f815e508d22
Wiliam Mendes <wiliammsa@gmail.com>
2020-09-04 02:08:38 -0300

    Ajuste de visibilidade do atributo table na model Character

--- www/api/app/Models/Character.php
+++ www/api/app/Models/Character.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Characters extends Model
+class Character extends Model
 {
     use SoftDeletes;
 
-    protected $table = 'characters';
+    public $table = 'character';
+
     protected $fillable = ['name', 'role', 'school', 'house', 'patronus'];
     protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
 }
