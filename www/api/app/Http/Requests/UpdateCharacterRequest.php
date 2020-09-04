453c11536e45b306ebebad62ea0eecf5fb309f0b
Wiliam Mendes <wiliammsa@gmail.com>
2020-09-04 02:13:20 -0300

    Implementação de FormRequests de armazenamento e alteração

---
+++ www/api/app/Http/Requests/UpdateCharacterRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCharacterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => "required|unique:character|max:100",
+            'role' => 'required|max:20',
+            'school' => 'required|max:100',
+            'house' => 'required|max:50',
+            'patronus' => 'required|max:100',
+        ];
+    }
+}
