2bf7ed1b05cdfc89227570c060a269f396cc8a89
Wiliam Mendes <wiliammsa@gmail.com>
2020-09-04 02:11:01 -0300

    Implementação da classe de serviço para consulta do id da casa

---
+++ www/api/app/Services/PotterApi/HousesService.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Services\PotterApi;
+
+use App\Exceptions\HouseNotFoundException;
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Support\Facades\Http;
+
+class HousesService
+{
+    const ENDPOINT = '/houses';
+
+    public static function getById(String $id)
+    {
+        $potterAPIUrl = env('POTTERAPI_URL');
+        $potterAPIAccessKey = env('POTTERAPI_ACCESS_KEY');
+
+        $url = $potterAPIUrl . self::ENDPOINT . "/{$id}?key={$potterAPIAccessKey}";
+        try {
+            $response = Http::get($url);
+            $response = $response->json();
+
+            if (isset($response[0]['name'])){
+                return true;
+            }
+
+            return false;
+        } catch (ClientException $exception) {
+            return $exception->getMessage();
+        }
+    }
+
+    public static function validateHouseId(String $id)
+    {
+
+        $house = HousesService::getById($id);
+        if (!$house){
+            throw new HouseNotFoundException('O ID da casa é inválido');
+        }
+    }
+}
